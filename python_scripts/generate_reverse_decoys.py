5deff001d40afd659db9fa20dcb32c37d68466e0
Michael Riffle <mriffle@uw.edu>
2024-05-06 12:48:35 -0700

    accept decoy prefix for generated decoys

--- python_scripts/generate_reverse_decoys.py
+++ python_scripts/generate_reverse_decoys.py
@@ -1,34 +1,39 @@
 import sys
+import argparse
 
 def reverse_sequence(sequence):
     return sequence[::-1]
 
-def print_entry(header, sequence):
+def print_entry(header, sequence, decoy_prefix):
     sequence = sequence.rstrip('*')
     print(f"{header}\n{sequence}")
-    print(f">DECOY_{header[1:]}\n{reverse_sequence(sequence)}")
+    print(f">{decoy_prefix}{header[1:]}\n{reverse_sequence(sequence)}")
 
-def process_fasta_file(input_file):
+def process_fasta_file(input_file, decoy_prefix):
     header = ""
     sequence = ""
-
     with open(input_file, 'r') as file_in:
         for line in file_in:
             if line.startswith('>'):
                 if header and sequence:
-                    print_entry(header, sequence)
+                    print_entry(header, sequence, decoy_prefix)
                 header = line.strip()
                 sequence = ""
             else:
                 sequence += line.strip()
+    if header and sequence:
+        print_entry(header, sequence, decoy_prefix)
 
-        if header and sequence:
-            print_entry(header, sequence)
+def main():
+    parser = argparse.ArgumentParser(description='Process FASTA file and generate decoy sequences.')
+    parser.add_argument('input_file', help='Input FASTA file')
+    parser.add_argument('--decoy_prefix', default='DECOY_', help='Decoy prefix (default: DECOY_)')
 
-# Check if the input file is provided as a command-line argument
-if len(sys.argv) < 2:
-    print("Please provide the input FASTA file as a command-line argument.")
-    sys.exit(1)
+    args = parser.parse_args()
+    input_file = args.input_file
+    decoy_prefix = args.decoy_prefix
 
-input_file = sys.argv[1]
-process_fasta_file(input_file)
+    process_fasta_file(input_file, decoy_prefix)
+
+if __name__ == '__main__':
+    main()
